70bb929b652004ab76ffd03cf9e08fa1a8c00c53
Lourdes Infante <117685514+LourInf@users.noreply.github.com>
2023-10-31 10:07:01 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,18 +3,25 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import notes from "../notes";
 
 //2. Create our component:
 function App() {
   //3. JS if needed
-  console.log("component App");
 
   //4. return html element
   return (
     <div>
       <Header />
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+
       <Footer />
-      <Note />
     </div>
   );
 }
